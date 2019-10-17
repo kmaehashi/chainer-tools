e3fbc85fb38382788a77bb8325d0fe44976d6645
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2019-10-17 17:16:15 +0900

    add jenkins tool

---
+++ jenkins/show_console_for_failed_shuffle.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree
+import urllib.request
+
+
+def get(url):
+    return xml.etree.ElementTree.fromstring(urllib.request.urlopen(url).read())
+
+def show(job):
+    matrix = get('https://jenkins.preferred.jp/job/chainer/job/{}/api/xml?xpath=/matrixProject/activeConfiguration[color="red"]/url&wrapper=result'.format(job))
+    for m in matrix:
+        print('open "{}lastBuild/consoleFull#footer"'.format(m.text))
+
+if __name__ == '__main__':
+    for repo in ['chainer', 'cupy']:
+        for branch in ['master', 'stable']:
+            job = 'daily_{}_{}-shuffled'.format(branch, repo)
+            print('### {}'.format(job))
+            show(job)
+            print('')
